4d3f7c9322f645abfff9c27c052fd4d673ce0055
talaberg <talaber@vipmail.hu>
2018-12-22 16:34:09 +0100

    OpenGl tutorial - 24: Setting up a Test Framework for OpenGL

--- OpenGLTutorial/src/main.cpp
+++ OpenGLTutorial/src/main.cpp
@@ -6,6 +6,8 @@
 #include "Shader.h"
 #include "Texture.h"
 
+#include "tests/TestClearColor.h"
+
 #include <GL/glew.h>
 #include <GL/glut.h>
 
@@ -40,6 +42,8 @@ glm::mat4 view;
 glm::vec3 translationA;
 glm::vec3 translationB;
 
+test::TestClearColor clearcolor_test;
+
 void onInitialize(void)
 {
     if (glewInit() != GLEW_OK)
@@ -49,48 +53,9 @@ void onInitialize(void)
 
     GLCALL(std::cout << glGetString(GL_VERSION) << std::endl;)
 
-    float positions[] = {
-        -50.0f, -50.0f, 0.0f, 0.0f,
-         50.0f, -50.0f, 1.0f, 0.0f,
-         50.0f,  50.0f, 1.0f, 1.0f,
-        -50.0f,  50.0f, 0.0f, 1.0f
-    };
-    unsigned int indices[] = {
-        0, 1, 2,
-        2, 3, 0
-    };
-
     GLCALL(glEnable(GL_BLEND));
     GLCALL(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
-    va = new VertexArray();
-    vb = new VertexBuffer(positions, 4 * 4 * sizeof(float));
-
-    VertexBufferLayout layout;
-    layout.Push<float>(2);
-    layout.Push<float>(2);
-
-    va->AddBuffer(*vb, layout);
-
-    ib = new IndexBuffer(indices, 6);
-
-    texture = new Texture("res/textures/chutulu.png");
-    texture->Bind(0);
-
-    shader = new Shader("res/shaders/Basic.shader");
-    shader->Bind();
-    shader->SetUniform1i("u_Texture", 0);
-
-    translationA = glm::vec3(200, 200, 0);
-    translationB = glm::vec3(400, 200, 0);
-
-    proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-    view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0 ,0));
-
-    va->Unbind();
-    vb->Unbind();
-    ib->Unbind();
-    shader->Unbind();
 
     renderer = new Renderer();
 
@@ -108,32 +73,20 @@ void onDisplay(void)
 {
     renderer->Clear();
 
+    clearcolor_test.OnUpdate(0.0f);
+    clearcolor_test.OnRender();
+
     // Start the Dear ImGui frame
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplFreeGLUT_NewFrame();
 
+    clearcolor_test.OnImGuiRender();
     imgui_display_code();
 
     // Rendering
     ImGui::Render();
     ImGuiIO& io = ImGui::GetIO();
     glViewport(0, 0, (GLsizei)io.DisplaySize.x, (GLsizei)io.DisplaySize.y);
-
-    {
-        glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-        MVP = proj * view * model;
-        shader->Bind();
-        shader->SetUniformMat4f("u_MVP", MVP);
-        renderer->Draw(*va, *ib, *shader);
-    }
-    {
-        glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-        MVP = proj * view * model;
-        shader->Bind();
-        shader->SetUniformMat4f("u_MVP", MVP);
-        renderer->Draw(*va, *ib, *shader);
-    }
-    shader->Unbind();
 
     glUseProgram(0); // You may want this if using this code in an OpenGL 3+ context where shaders may be bound, but prefer using the GL3+ code.
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
@@ -145,26 +98,6 @@ void onDisplay(void)
 
 void onIdle()
 {
-    static long last_time = 0;
-    static long dt = 0;
-    long time = glutGet(GLUT_ELAPSED_TIME); // elapsed time since the start of the program
-    dt += time - last_time;
-    last_time = time;
-
-    if (dt > 100)
-    {
-        static float increment = 0.05f;
-        if (r > 1.0f)
-            increment = -0.05f;
-        else if (r < 0.0f)
-            increment = 0.05f;
-
-        r += increment;
-
-        dt = 0;
-        glutPostRedisplay();                    // redraw the scene
-    }
-
 }
 
 int main(int argc, char** argv)
